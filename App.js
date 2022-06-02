45fdd5ca7e6e36961f267b8e71b15c47d0d61ae2
JRafael <rafaelmatiashue@gmail.com>
2022-06-02 10:17:12 -0300

    Tela de login, Tela de Cadastro

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
+import React, { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,  View } from 'react-native';
+import { Button, Input, Text } from "react-native-elements";
+import  Icon  from "react-native-vector-icons/FontAwesome";
+import styles from "./style/MainStyle";
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Login from "./telas/Login";
+import Principal from "./telas/Principal"
+import Cadastro from "./telas/Cadastro";
 
-export default function App() {
+
+const Stack = createStackNavigator();
+
+function MyStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Principal" component={Principal} />
+      <Stack.Screen name="Cadastro" component={Cadastro} />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
